6d065339a23e128ab0c11a78ab978a8c5924c56e
practical3 <chinyy-sa16@student.tarc.edu.my>
2018-10-30 09:47:45 +0800

    commit

---
+++ app/src/main/java/android/example/com/hellotoast/MainActivity.java
@@ -0,0 +1,22 @@
+package android.example.com.hellotoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+    }
+}
